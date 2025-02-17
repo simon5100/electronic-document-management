f3d6cfcbf6bcb13019cfddce5782d82b914860e3
simon5100 <simon51005100@gmail.com>
2025-02-18 00:52:27 +0700

    Добавляем проект в github

---
+++ court_site_parser/src/main/java/com/polikarpov/ums/App.java
@@ -0,0 +1,13 @@
+package com.polikarpov.ums;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
