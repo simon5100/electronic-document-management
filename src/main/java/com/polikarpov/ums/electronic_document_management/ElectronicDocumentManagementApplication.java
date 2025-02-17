511d324378d07f9236e48558beedf4b5f50c1ec3
simon5100 <simon51005100@gmail.com>
2025-02-17 23:38:23 +0700

    Добавляем проект в github

---
+++ src/main/java/com/polikarpov/ums/electronic_document_management/ElectronicDocumentManagementApplication.java
@@ -0,0 +1,17 @@
+package com.polikarpov.ums.electronic_document_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicDocumentManagementApplication {
+
+    public static void main(String[] args) {
+        // Запуск Spring Boot приложения.
+        // Этот метод является точкой входа в приложение.
+        // Дополнительная логика будет добавлена по мере разработки.
+        SpringApplication.run(ElectronicDocumentManagementApplication.class, args);
+
+
+    }
+}
---
+++ src/test/java/com/polikarpov/ums/electronic_document_management/ElectronicDocumentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polikarpov.ums.electronic_document_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicDocumentManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
